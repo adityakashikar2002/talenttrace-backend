882fbe98351a81651c326dfc1168ae72375c5be9
Aditya Kashikar <150321037+adityakashikar2002@users.noreply.github.com>
2024-11-24 20:05:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -234,4 +234,4 @@ def download_excel():
     return send_file(EXCEL_FILE, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000,debug=True)
